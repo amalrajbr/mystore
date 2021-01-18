18246d2fc95d65ce9c85d41ba46e931c205725a6
amalrajbr <amalrajbrr@gmail.com>
2021-01-18 01:15:45 -0500

    Added Mnesia Table List
    Added list of tables as private define to indicate the active Mnesia tables to be present in the application.

--- include/my_image_repository.hrl
+++ include/my_image_repository.hrl
@@ -1,6 +1,6 @@
 % LOGGING PRIVATE DEFINE
--define(LOG_DIR(X), code:lib_dir(my_image_repository_app) ++ "/logs/log-" ++ X ++ "_1.txt").
--define(LOG_DIR_N(X), code:lib_dir(my_image_repository_app) ++ "/logs/log-" ++ X ++ ".txt").
+-define(LOG_DIR(X), code:lib_dir(my_image_repository) ++ "/logs/log-" ++ X ++ "_1.txt").
+-define(LOG_DIR_N(X), code:lib_dir(my_image_repository) ++ "/logs/log-" ++ X ++ ".txt").
 
 % DEBUGGING PRIVATE DEFINE
 -define(PRINT(X), io:format("\r\n~p\r\n",[X]).
@@ -14,3 +14,5 @@
 					, raw_image_size
 					, b64_image_size
 					}).
+					
+-define(TABLE_LIST, [image_data]).
